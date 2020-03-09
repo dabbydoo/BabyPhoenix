8d7954a9c3e9c9e9f948e53b9797741658a3dffa
Mrinank <mrinank.sivakumar@ontariotechu.net>
2020-03-09 00:07:04 -0400

    FMOD Integration
    Core API

--- SpriteLib3.0-ProjectSoul/SpriteLib3.0-Complete/SpriteLib3.0-Complete/main.cpp
+++ SpriteLib3.0-ProjectSoul/SpriteLib3.0-Complete/SpriteLib3.0-Complete/main.cpp
@@ -1,10 +1,14 @@
 #include "Game.h"
+#include "GPCSound.h"
 
 #undef main
 int main()
 {
 	//Create the Game
 	Game theGame;
+	SoundManager::init("./assets/sounds/");
+	Sound2D _test("Sound.wav", "group1");
+	_test.play();
 
 	//Initialize the game
 	theGame.InitGame();
